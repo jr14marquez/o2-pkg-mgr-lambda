a2dcc6b90079e58a7ca5c0392766af64ba22f660
Raul Marquez - WEST - RT 791011 <rmarquez@pu00cenwks258.vdi.ux.dg.local>
2018-11-08 11:50:05 -0700

    fix handler code to handle promises.. should probably look into async/await

--- handler.js
+++ handler.js
@@ -21,6 +21,9 @@ const apps = {
 
 module.exports.hello = (event, context, callback) => {
 	console.log('params passed are: ', event.queryStringParameters)
+	console.log('context: ',context.callbackWaitsForEmptyEventLoop)
+	context.callbackWaitsForEmptyEventLoop = false;
+	console.log('context: ',context)
 	
 	// if app name is passed look it up
 	if(event.queryStringParameters && event.queryStringParameters.app) {
@@ -31,56 +34,38 @@ module.exports.hello = (event, context, callback) => {
 			var group = event.queryStringParameters.group
 
 			util.getConfs(s3,app)
-			util.getSnapshotUrl(app)
+			util.getSnapshotData(app)
 			.then(data => {
 				console.log('after getting snapshot url with data: ',data)
 				// check s3 if there is already an rpm matching same params and latest snapshot
 				var key = `rpms/${user}:${group}${home}/${app.alias}-${data.version}-${data.timestamp}.${data.buildNumber}.noarch.rpm`
-				console.log('key: ',key)
-				s3.headObject({Bucket: process.env.BUCKET, Key: key })
-				.on('success', (data) => {
-					console.log('succes: rpm file exists')
-					// send back url 
-					var res = {
-						statusCode: 200,
-						body: JSON.stringify({ file: `${data.request.params.Key.split('/').pop()}`, url: `https://s3.amazonaws.com/${process.env.BUCKET}/${data.request.params.Key}` })
-					}
-					callback(null, res)	
-				})
-				.on('error', (err) => {
-					if(err.code == 'NotFound'){
-						// rpm doesn't exists so we go through build process and create one
-						Axios({ method: 'GET', url: data.url, responseType: 'stream'})
-					  .then(response => {
-					    const path = Path.resolve('/','tmp',data.jarFile)
-					    // pipe the result stream into a file
-					    response.data.pipe(Fs.createWriteStream(path))
-					    response.data.on('error', (err) => console.log('error downloading: ',app.artifactId)) 
-					    response.data.on('end', () => {
+				return util.headObject(s3, key, data)
+			})
+			.then(data => {
+				if(data.found == true) {
+					callback(null, { statusCode: 200, body: JSON.stringify({ url: data.url }) })
+				}
+				else {
+					return util.getSnapshot(data.snapdata)
+				}
+			})
+			.then(snapdata => {
+				if(!snapdata) return;
+				var rpmApp = { name: app.alias, user: user, home: home, group: group, version: snapdata.version, release: `${snapdata.timestamp}-${snapdata.buildNumber}`, jar: snapdata.jarFile}
+				return rpm.build(rpmApp)
+			})
+			.then(rpmdata => {
+				if(!rpmdata) return;
+				var params = {Bucket: process.env.BUCKET, Key: `rpms/${user}:${group}${home}/${rpmdata.split('/')[2]}`, Body: Fs.createReadStream(rpmdata)};
+				return s3.upload(params).promise()
+			})
+			.then(uploadData => {
+				if(!uploadData) return;
+				var res = { statusCode: 200, body: JSON.stringify({ message: uploadData }) }
+				callback(null, res)	
+			})
+			.catch(err => onError(err))
 
-					      var rpmApp = { name: app.alias, user: user, home: home, group: group, version: data.version, release: `${data.timestamp}-${data.buildNumber}`, jar: data.jarFile}
-					      rpm.build(rpmApp)
-					      .then(result => {
-					      	console.log('rpm result: ', result)
-
-					      	var params = {Bucket: process.env.BUCKET, Key: `rpms/${user}:${group}${home}/${result.split('/')[2]}`, Body: Fs.createReadStream(result)};
-									s3.upload(params)
-										send(function(err,data) { 
-											var res = {
-												statusCode: 200,
-												body: JSON.stringify({ message: data })
-											}
-											callback(null, res)	
-										})
-									  				
-					      })
-					      
-					    })
-					  })
-					  .catch(err => callback(null,onError(err)))
-					}  
-				}).send()
-			})	
 
 		} else{
 			// throw error app doesnt exist
@@ -92,6 +77,8 @@ module.exports.hello = (event, context, callback) => {
 
 };
 
+
 function onError(error) {
+	console.log('error occured: ',error)
 	return { statusCode: 200, body: JSON.stringify({ message: `Error returned: ${error}` }) }
 }
